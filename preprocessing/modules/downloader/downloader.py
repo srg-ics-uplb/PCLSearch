5e34183f970fd6469bd91cad28db9c1705a82cf2
Joseph Anthony C. Hermocilla <jachermocilla@gmail.com>
2017-07-14 11:56:49 +0800

    edits

--- preprocessing/modules/downloader/downloader.py
+++ preprocessing/modules/downloader/downloader.py
@@ -12,15 +12,28 @@ class PCLSearchDownloader:
 class GoogleDriveDownloader(PCLSearchDownloader):
 
     def download(self,url,target_path):
+        if "open" in url:
+                data=url.split('=')
+                data=data[1].split('&')
+                docid=data[0]
+        elif "viewer" in url:
+                data=url.split('=')
+                docid=data[3]
+        else:
+                data=url.split('/')
+                docid=data[5]
+
+        print docid
+        print target_path
+
         download_url = "https://docs.google.com/uc?export=download"
         session = requests.Session()
-        id = url.split('/')[5]
+        id = docid
         response = session.get(download_url, params = { 'id' : id }, stream = True)
         token = self.get_confirm_token(response)
         if token:
            params = { 'id' : id, 'confirm' : token }
            response = session.get(URL, params = params, stream = True)
-
         self.save_response_content(response, target_path)
 
     def get_confirm_token(self,response):
@@ -32,15 +45,46 @@ class GoogleDriveDownloader(PCLSearchDownloader):
 
     def save_response_content(self,response,target_path):
         CHUNK_SIZE = 32768
-
         with open(target_path, "wb") as f:
             for chunk in response.iter_content(CHUNK_SIZE):
                 if chunk: # filter out keep-alive new chunks
                     f.write(chunk)
 
 
-with open('../../pclsearch.json') as config_file:
-    config = json.load(config_file)
-print config['pclsearch']['pdfs_root']
-downloader = GoogleDriveDownloader()
-downloader.download("https://drive.google.com/file/d/0BxI8feCZhWsobHdYSWVJX0h5WGM/view",config['pclsearch']['pdfs_root']+"/test.pdf")
+def download_all():
+    with open('../../pclsearch.json') as config_file:
+        config = json.load(config_file)
+
+    sources = config['sources']
+
+    for source in sources:
+        source_type=source['type']
+        source_name=source['name']
+        source_year=source['year']
+        spider_class=source['spider_class']
+        start_url=source['start_url']
+
+        #source may require a different downloader later
+        downloader=GoogleDriveDownloader()
+
+        download_path=config['pclsearch']['pdfs_root']+"/"+source_type+"/"+source_name+"/"+source_year
+        urls = download_path+"/"+source_name+"-"+source_year+".url"
+        #print urls
+        f = open(urls)
+        i=-1
+        for line in f:
+            i = i+1
+            if (i==0):
+                continue
+            else:
+                #print line
+                downloader.download(line,download_path+"/"+str(i)+".pdf")
+         
+
+
+download_all()
+#with open('../../pclsearch.json') as config_file:
+#    config = json.load(config_file)
+#print config['pclsearch']['pdfs_root']
+#downloader = GoogleDriveDownloader()
+#downloader.download("https://drive.google.com/file/d/0BxI8feCZhWsobHdYSWVJX0h5WGM/view",config['pclsearch']['pdfs_root']+"/test.pdf")
