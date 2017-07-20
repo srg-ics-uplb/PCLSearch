0b7a94b391dcd4edc88fc992f001e6959d887371
Joseph Anthony C. Hermocilla <jachermocilla@gmail.com>
2017-07-20 10:38:33 +0800

    edits

--- angular-client/src/app/app.component.ts
+++ angular-client/src/app/app.component.ts
@@ -9,6 +9,7 @@ import 'rxjs/add/operator/map';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent implements OnInit {
   title = 'app works!';
 
